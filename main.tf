0c50d2b0d8659e556eebb9da6dfbe7cb6615220a
Jacek <jacpawlyszyn@task.gda.pl>
2025-04-12 14:15:26 +0200

    add: gitignore

--- main.tf
+++ main.tf
@@ -2,31 +2,22 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "my_resource_group" {
-  name     = "tfstate-rg"
-  location = "North Europe"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "mytfstatestorage"
-  resource_group_name       = azurerm_resource_group.my_resource_group.name
-  location                 = azurerm_resource_group.my_resource_group.location
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name       = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "my_container" {
-  name                  = "tfstate-container"
-  storage_account_name  = azurerm_storage_account.my_storage_account.name
+resource "azurerm_storage_container" "example" {
+  name                  = "example-container"
+  storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = azurerm_resource_group.my_resource_group.name
-    storage_account_name = azurerm_storage_account.my_storage_account.name
-    container_name       = azurerm_storage_container.my_container.name
-    key                  = "terraform.tfstate"
-  }
 }
 
