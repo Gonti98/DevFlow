2870aae2166b18925d1110047e43dd0afa1323d2
Jacek <jacpawlyszyn@task.gda.pl>
2025-04-13 11:24:58 +0200

    new setup test

---
+++ infrastructure/storage_backend.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_backend" {
+  name     = "terraform-backend-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "tf_backend" {
+  name                     = "mytfbackendstorage001"
+  resource_group_name      = azurerm_resource_group.tf_backend.name
+  location                 = azurerm_resource_group.tf_backend.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tf_state" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tf_backend.id
+  container_access_type = "private"
+}
+
